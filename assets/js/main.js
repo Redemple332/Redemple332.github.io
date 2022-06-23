38b66729d1151f2a012ec63f79607c44c143ccb7
Redemple32 <marceloredemple332@gmail.com>
2022-06-23 11:38:24 +0800

    about page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader() {
+    const header = document.getElementById('header');
+    //When the scroll is greater than 50 viewport height, and the scroll-header class to the header tagName
+    if(this.scrollY >= 50) header.classList.add('scroll-header');else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 
